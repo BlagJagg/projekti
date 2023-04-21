14a2fe9948b131038bb1d96e9689fb27a301f276
Flashyyyyyy <mantere0901@gmail.com>
2023-04-21 12:16:39 +0300

    added physics and jumping

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5.0f;
-    [SerializeField] private float jumpPower = 5.0f;
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2.0f;
 
     private Rigidbody2D _playerRigidbody;
+    private bool _isGrounded;
     private void Start()
     {
         _playerRigidbody = GetComponent<Rigidbody2D>();
@@ -17,22 +20,50 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         MovePlayer();
-
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
             Jump();
+        }
+
+        ApplyFallMultiplier();
     }
     private void MovePlayer()
     {
         var horizontalInput = Input.GetAxisRaw("Horizontal");
         _playerRigidbody.velocity = new Vector2(horizontalInput * playerSpeed, _playerRigidbody.velocity.y);
     }
-    private void Jump() => _playerRigidbody.velocity = new Vector2(0, jumpPower);
-
-    private bool IsGrounded()
+    private void Jump()
     {
-        var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.7f);
-        return groundCheck.collider != null && groundCheck.collider.CompareTag("Ground");
+        _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x, jumpForce);
+        _isGrounded = false;
+    }
+    private void ApplyFallMultiplier()
+    {
+        if (_playerRigidbody.velocity.y < 0)
+        {
+            _playerRigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (_playerRigidbody.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            _playerRigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+        }
+    }
 }
+
