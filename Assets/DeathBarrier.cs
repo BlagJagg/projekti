2b240e0cf16da67fef82b404d2a8490c23b1b671
Flashyyyyyy <mantere0901@gmail.com>
2023-05-29 18:31:48 +0300

    Commented DeathBarrier

--- Assets/DeathBarrier.cs
+++ Assets/DeathBarrier.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class DeathBarrier : MonoBehaviour
 {
-    [SerializeField] private float deathYLevel = -10f;
+    [SerializeField] private float deathYLevel = -10f; // The Y level at which the player should die
 
     private void Update()
     {
-        if (transform.position.y < deathYLevel)
+        if (transform.position.y < deathYLevel) // Check if player's Y position is below the deathYLevel
         {
-            SceneManager.LoadScene("Menu");
+            SceneManager.LoadScene("Menu"); // Load the "Menu" scene
         }
     }
 }
