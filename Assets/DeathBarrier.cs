dbbb0c492d5a531faf0fdcaf4ff9c09cc367059e
Flashyyyyyy <mantere0901@gmail.com>
2023-05-29 18:40:54 +0300

    added high score and made the score system better

--- Assets/DeathBarrier.cs
+++ Assets/DeathBarrier.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class DeathBarrier : MonoBehaviour
 {
     [SerializeField] private float deathYLevel = -10f; // The Y level at which the player should die
+    [SerializeField] private GameObject player; // Reference to the player object
 
     private void Update()
     {
-        if (transform.position.y < deathYLevel) // Check if player's Y position is below the deathYLevel
+        if (player.transform.position.y < deathYLevel) // Check if player's Y position is below the deathYLevel
         {
+            Destroy(player); // Delete the player object
             SceneManager.LoadScene("Menu"); // Load the "Menu" scene
         }
     }
--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -2,35 +2,31 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    private Transform player;        // Reference to the player's transform
-    private float highestYPosition;  // The highest Y position achieved by the player
-    private int score;               // The current score
+    private Transform player;         // Reference to the player's transform
+    private float currentYPosition;   // The current Y position of the player
+    private int highScore;            // The saved high score
 
     void Start()
     {
         // Find the player object using the "Player" tag and store its transform
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
-        // Initialize the highestYPosition to the player's initial Y position
-        highestYPosition = player.position.y;
+        // Initialize the currentYPosition to the player's initial Y position
+        currentYPosition = player.position.y;
 
-        // Initialize the score to zero
-        score = 0;
+        // Load the saved high score from PlayerPrefs
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     void Update()
     {
         // Get the current Y position of the player
-        float currentYPosition = player.position.y;
+        float newCurrentYPosition = player.position.y;
 
-        // If the current position is higher than the previous highest position
-        if (currentYPosition > highestYPosition)
+        // If the new position is lower than the current position, update the current position
+        if (newCurrentYPosition < currentYPosition)
         {
-            // Update the highestYPosition to the current position
-            highestYPosition = currentYPosition;
-
-            // Round the highestYPosition to an integer value and set it as the score
-            score = Mathf.RoundToInt(highestYPosition);
+            currentYPosition = newCurrentYPosition;
         }
     }
 
@@ -45,8 +41,27 @@ public class ScoreManager : MonoBehaviour
         // Set the text color of the style to black
         style.normal.textColor = Color.black;
 
-        // Display the score label on the screen using the GUI.Label function
-        // The label will be positioned at (10, 10) with a size of (100, 30)
-        GUI.Label(new Rect(10, 10, 100, 30), "Score: " + score, style);
+        // Display the current score label on the screen using the GUI.Label function
+        // The label will be positioned at (10, 10) with a size of (200, 30)
+        GUI.Label(new Rect(10, 10, 200, 30), "Score: " + Mathf.RoundToInt(currentYPosition), style);
+
+        // Display the high score label on the screen using the GUI.Label function
+        // The label will be positioned below the current score label with a size of (200, 30)
+        GUI.Label(new Rect(10, 40, 200, 30), "High Score: " + highScore, style);
+    }
+
+    void OnDestroy()
+    {
+        // Check if the current score is higher than the saved high score
+        int currentScore = Mathf.RoundToInt(currentYPosition);
+        if (currentScore > highScore)
+        {
+            // Update the high score
+            highScore = currentScore;
+
+            // Save the new high score to PlayerPrefs
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        }
     }
 }
