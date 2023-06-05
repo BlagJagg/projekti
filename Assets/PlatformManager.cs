d69e6c827b242691d2bf3c2513d5930fa42a5d91
rs0642 <salminennrasmus@gmail.com>
2023-06-05 13:13:00 +0300

    ff
    ff

--- Assets/PlatformManager.cs
+++ Assets/PlatformManager.cs
@@ -13,11 +13,13 @@ public class PlatformManager : MonoBehaviour
 
     private float timer;                    // Timer for platform spawning
     private Vector2 lastPlatformPosition;   // Position of the last spawned platform
+    private int platformCount = 0;          // Count of spawned platforms
 
     void Start()
     {
         timer = platformSpawnInterval;                // Initialize the timer
         lastPlatformPosition = transform.position;    // Set the last platform position to the initial position of the PlatformManager
+        SpawnPlatform();
     }
 
     void Update()
@@ -46,5 +48,61 @@ public class PlatformManager : MonoBehaviour
         // Update the last platform position to the position of the newly spawned platform
         lastPlatformPosition = newPlatform.transform.position;
 
+        platformCount++;
+
+        if (platformCount % 3 == 0)
+        {
+            if (Random.Range(0, 2) == 0)
+            {
+                // Add the moving platform component to the spawned platform
+                newPlatform.AddComponent<MovingPlatform>();
+            }
+            else
+            {
+                // Add the self-destruct platform component to the spawned platform
+                SelfDestructPlatform selfDestruct = newPlatform.AddComponent<SelfDestructPlatform>();
+                selfDestruct.destroyDelay = 1f;
+            }
+        }
+    }
+}
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float movementDistance = 2f;   // Distance the platform moves horizontally
+    public float movementSpeed = 2f;      // Speed at which the platform moves
+
+    private Vector2 initialPosition;      // Initial position of the platform
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Calculate the target position based on the movement distance and speed
+        float targetX = initialPosition.x + Mathf.PingPong(Time.time * movementSpeed, movementDistance);
+        Vector2 targetPosition = new Vector2(targetX, transform.position.y);
+
+        // Move the platform towards the target position
+        transform.position = targetPosition;
+    }
+}
+
+public class SelfDestructPlatform : MonoBehaviour
+{
+    public float destroyDelay = 1f;   // Delay before self-destructing the platform
+
+    private float timer = 0f;        // Timer for self-destruction
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= destroyDelay)
+        {
+            Destroy(gameObject);
+        }
     }
 }
